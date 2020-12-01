5535e3bd69cfc016f9d55e9096f4d67db63db4d8
Eniyan27 <74698190+Eniyan27@users.noreply.github.com>
2020-12-01 14:09:20 +0530

    BLE upd

--- src/components/BLE.js
+++ src/components/BLE.js
@@ -1,11 +1,16 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {ListItem} from 'react-native-elements';
 import BluetoothSerial from 'react-native-bluetooth-serial';
 const BLE = () => {
   const [device, setDevice] = useState([]);
   const [bstate, setBstate] = useState(false);
-  BluetoothSerial.list()
+  const connect = (id) => {
+    BluetoothSerial.connect(id)
+      .then((res) => console.log(res, 'connected'))
+      .catch((err) => console.log(err));
+  };
+  /*BluetoothSerial.list()
     .then((res) => setDevice(res))
     .catch((err) => console.error(err));
   BluetoothSerial.isEnabled()
@@ -13,17 +18,24 @@ const BLE = () => {
     .catch((err) => console.error(err));
   BluetoothSerial.discoverUnpairedDevices()
     .then((res) => console.log(res))
+    .catch((err) => console.log(err));*/
+  const macid ="FC:A8:9A:00:12:2E"
+  BluetoothSerial.connect(macid)
+    .then((res) => console.log(res, 'connected'))
     .catch((err) => console.log(err));
-  const readDevice = async () => {
-    const response = await BluetoothSerial.readFromDevice();
-    return response;
-  };
-  readDevice()
+  // const readDevice = async () => {
+  //   const response = await BluetoothSerial.readFromDevice();
+  //   return response;
+  // };
+  // readDevice()
+  //   .then((res) => console.log(res))
+  //   .catch((err) => console.log(err));
+  BluetoothSerial.readFromDevice()
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
   return (
     <View>
-      {bstate ? (
+      {/* {bstate ? (
         <Text>Bluetooth is enabled</Text>
       ) : (
         <Text>Enable bluetooth to continue</Text>
@@ -31,8 +43,10 @@ const BLE = () => {
       {device.map((dev) => (
         <ListItem key={dev.id}>
           <Text>{dev.name}</Text>
+          <Text>{dev.address}</Text>
         </ListItem>
-      ))}
+      ))} */}
+      <Button onPress={() => connect(macid)} title="Click to Connect" />
     </View>
   );
 };
